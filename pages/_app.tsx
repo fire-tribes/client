16491b64c0c863c241cb2a717b1b7b0329eda5ed
YeongMin Ahn <ahn0min95@gmail.com>
2023-08-23 17:07:46 +0900

    chore: setting jotai

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ import theme from '../styles/theme';
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </ThemeProvider>
   );
 }
