c336c52aa6b9259216bdf6887277024b6bc3a0fd
YeongMin Ahn <ahn0min95@gmail.com>
2023-08-24 16:47:33 +0900

    chore: android용 service worker 파일 추가

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 
-const withPlugins = require('next-compose-plugins');
-const withPWA = require('next-pwa');
+// const withPlugins = require('next-compose-plugins');
+// const withPWA = require('next-pwa');
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+});
 
 const nextConfig = {
   reactStrictMode: true,
@@ -12,17 +16,20 @@ const nextConfig = {
   },
 };
 
-module.exports = withPlugins(
-  [
-    [
-      withPWA,
-      {
-        pwa: {
-          dest: 'public',
-        },
-      },
-    ],
-    // 추가 플러그인 작성
-  ],
+module.exports = withPWA({
   nextConfig,
-);
+});
+// module.exports = withPlugins(
+//   [
+//     [
+//       withPWA,
+//       {
+//         pwa: {
+//           dest: 'public',
+//         },
+//       },
+//     ],
+//     // 추가 플러그인 작성
+//   ],
+//   nextConfig,
+// );
