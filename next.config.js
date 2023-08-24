d85d970fd1e686f8ed0368f025922b92bef81323
YeongMin Ahn <ahn0min95@gmail.com>
2023-08-24 16:52:40 +0900

    remove: 불필요한 next-compose-plugins 제거

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
 
-// const withPlugins = require('next-compose-plugins');
-// const withPWA = require('next-pwa');
-
 const withPWA = require('next-pwa')({
   dest: 'public',
 });
@@ -19,17 +16,3 @@ const nextConfig = {
 module.exports = withPWA({
   nextConfig,
 });
-// module.exports = withPlugins(
-//   [
-//     [
-//       withPWA,
-//       {
-//         pwa: {
-//           dest: 'public',
-//         },
-//       },
-//     ],
-//     // 추가 플러그인 작성
-//   ],
-//   nextConfig,
-// );
