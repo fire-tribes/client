24628886fed288ce8de40e565a600ebb6c9b70ad
0min <ahn0min95@gmail.com>
2023-08-28 18:23:43 +0900

    Design: emotion theme palette 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,15 @@
-import theme from '../styles/theme';
 import { globalStyle } from '@/styles/global';
-import { Global, ThemeProvider } from '@emotion/react';
+import { useEmotionTheme } from '@/hook/useThemeHooks';
+import { Global, ThemeProvider as EmotionThemeProvider } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { useState } from 'react';
 import type { AppProps } from 'next/app';
+// import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles';
+
+// const muiTheme = createTheme({
+//   pal: theme.palette,
+// });
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [queryClient] = useState(
@@ -17,14 +22,18 @@ function MyApp({ Component, pageProps }: AppProps) {
     }),
   );
 
+  const emotionTheme = useEmotionTheme();
+
   return (
     <>
       <Global styles={globalStyle} />
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={theme}>
+        <EmotionThemeProvider theme={emotionTheme}>
+          {/* <MuiThemeProvider theme={muiTheme}> */}
           <ReactQueryDevtools />
           <Component {...pageProps} />
-        </ThemeProvider>
+          {/* </MuiThemeProvider> */}
+        </EmotionThemeProvider>
       </QueryClientProvider>
     </>
   );
