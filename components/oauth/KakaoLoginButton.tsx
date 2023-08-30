28c8b24e2711985aeb5deb0a0b62a9b60be4d0f9
YeongMin Ahn <ahn0min95@gmail.com>
2023-08-30 16:41:04 +0900

    feat: 카카오 로그인 버튼 구현

---
+++ components/oauth/KakaoLoginButton.tsx
@@ -0,0 +1,20 @@
+import { useKakaoLogin } from '@/hook/useKakaoLogin';
+import { useEffect } from 'react';
+
+export const KakaoLoginButton = () => {
+  const { init, open } = useKakaoLogin();
+  // init을 진행
+
+  useEffect(() => {
+    init();
+  }, [init]);
+
+  return (
+    <a id="kakao_login_button" onClick={open}>
+      <img
+        src="images/kakao_login/ko/kakao_login_medium_narrow.png"
+        alt="카카오 로그인 버튼"
+      />
+    </a>
+  );
+};
