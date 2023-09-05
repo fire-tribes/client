8575843c6877e96f59dfcf5594e1e052af3b9acb
Whale2200d <skatewang99@gmail.com>
2023-09-06 01:22:46 +0900

    Rename : figma에 기록된 URL에 따라 파일 및 폴더 위치 및 이름 변경

---
+++ pages/fires/main/empty/index.tsx
@@ -0,0 +1,177 @@
+import Layout from '@/components/common/Layout';
+import AlertModal from '@/components/common/AlertModal';
+import { useRouter } from 'next/router';
+
+function empty() {
+  const router = useRouter();
+  const onMoveSearchPage = () => {
+    router.push('/fires/search');
+  };
+  return (
+    <Layout>
+      <AlertModal
+        type={'confirm'}
+        title={'준비중입니다.'}
+        message={
+          '4대 보험을 제한 결과값 도출은 곧 출시 예정입니다. 조금만 기다려주세요!'
+        }
+      />
+      <AlertModal
+        type={'alert'}
+        title={'최근 검색어 삭제'}
+        message={'최근 검색어를 모두 삭제하시겠어요?'}
+      />
+      <div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+        <div>돈주머니 이미지</div>
+        <div>
+          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+        </div>
+        <div>
+          <button onClick={onMoveSearchPage}>+ 주식 추가하기</button>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default empty;
---
+++ pages/fires/search.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function search() {
+  return (
+    <div>
+      <input type="text" placeholder="주식 이름 혹은 티커 검색" />
+    </div>
+  );
+}
+
+export default search;
