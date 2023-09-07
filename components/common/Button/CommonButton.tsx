47163d15517be96181ca2f26717963d55bc60779
YeongMin Ahn <ahn0min95@gmail.com>
2023-09-07 23:23:15 +0900

    feat: commonButton 이전으로 복귀

--- components/common/Button/CommonButton.tsx
+++ components/common/Button/CommonButton.tsx
@@ -1,32 +1,27 @@
-// import { ButtonUI } from './style';
-// import { Button, ButtonProps, styled } from '@mui/material';
-// {
-//   children,
-//   color = 'primary',
-//   variant = 'contained',
-//   ...rest
-// }: ButtonProps
+import { Button, ButtonProps, styled } from '@mui/material';
 
-import { PropsWithChildren } from 'react';
-
-const CommonButton = ({ children }: PropsWithChildren) => {
+const CommonButton = ({
+  children,
+  color = 'primary',
+  variant = 'contained',
+  ...rest
+}: ButtonProps) => {
   return (
-    // <StyledButton color={color} variant={variant} {...rest}>
-    //   {children}
-    // </StyledButton>
-    <div>{children}</div>
+    <StyledButton color={color} variant={variant} {...rest}>
+      {children}
+    </StyledButton>
   );
 };
 
-// const StyledButton = styled(Button)`
-//   text-transform: none;
+const StyledButton = styled(Button)`
+  text-transform: none;
 
-//   font-weight: 300;
-//   box-shadow: none;
+  font-weight: 300;
+  box-shadow: none;
 
-//   &:hover {
-//     box-shadow: none;
-//   }
-// `;
+  &:hover {
+    box-shadow: none;
+  }
+`;
 
 export default CommonButton;
