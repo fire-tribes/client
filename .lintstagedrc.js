22f630d785c259d05c1e844f730919d96af965c9
YeongMin Ahn <ahn0min95@gmail.com>
2023-09-08 12:47:03 +0900

    refactor: 불필요한 주석제거

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -2,7 +2,6 @@
 const path = require('path');
 
 const buildEslintCommand = (filenames) => {
-  console.log(filenames);
   return `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`;
