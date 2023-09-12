e9b4979636f3a1e918c65341f862b204e34a5d5d
YeongMin Ahn <ahn0min95@gmail.com>
2023-09-12 18:31:05 +0900

    feat: kakao server와 통신하는 api 구현

--- core/api/kakao.ts
+++ core/api/kakao.ts
@@ -1,28 +1,5 @@
 import axios from 'axios';
-
-const kakaoOauthInstance = axios.create({
-  baseURL: 'https://kauth.kakao.com/oauth/',
-});
-
-export const kakaoAPI = {
-  getAccessToken: (code: string) => {
-    return kakaoOauthInstance.post<KakaoGetAccessTokenReponse>(
-      'token',
-      {},
-      {
-        headers: {
-          'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
-        },
-        params: {
-          grant_type: 'authorization_code',
-          client_id: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY,
-          redirect_uri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
-          code: code,
-        },
-      },
-    );
-  },
-};
+import type { KakaoDefaultUserDataModel } from '@/@types/models/kakao';
 
 type KakaoGetAccessTokenReponse = {
   access_token: string;
@@ -30,4 +7,44 @@ type KakaoGetAccessTokenReponse = {
   refresh_token: string;
   expires_in: number;
   refresh_token_expires_in: number;
+};
+
+const kakaoOauthInstance = axios.create();
+const KAKAO_API_URLS = {
+  KAUTH: 'https://kauth.kakao.com/oauth',
+  KAPI_V2: 'https://kapi.kakao.com/v2',
+};
+const POST_CONTENT_TYPE = 'application/x-www-form-urlencoded;charset=utf-8';
+
+export const kakaoAPI = {
+  getAccessToken: (code: string) => {
+    return kakaoOauthInstance.post<KakaoGetAccessTokenReponse>(
+      `${KAKAO_API_URLS.KAUTH}/token`,
+      {},
+      {
+        headers: {
+          'Content-type': POST_CONTENT_TYPE,
+        },
+        params: {
+          grant_type: 'authorization_code',
+          client_id: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY,
+          redirect_uri:
+            process.env.NEXT_PUBLIC_KAKAO_REDIRECT_FRONT_SERVER_URI_TEST,
+          code: code,
+        },
+      },
+    );
+  },
+  getUserData: (accessToken: string) => {
+    return kakaoOauthInstance.post<KakaoDefaultUserDataModel>(
+      `${KAKAO_API_URLS.KAPI_V2}/user/me`,
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+          'Content-type': POST_CONTENT_TYPE,
+        },
+      },
+    );
+  },
 };
