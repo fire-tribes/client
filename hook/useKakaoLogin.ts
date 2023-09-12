3ca5a88a4876163315d1780aadf5666dc6102391
YeongMin Ahn <ahn0min95@gmail.com>
2023-09-12 18:36:03 +0900

    feat: 카카오 oauth를 이용하기 위한 redirectURL 변경

--- hook/useKakaoLogin.ts
+++ hook/useKakaoLogin.ts
@@ -1,10 +1,4 @@
-import { kakaoAPI } from '@/core/api/kakao';
-import { SignApi } from '@/core/api/oauth';
 import { useEffect, useState } from 'react';
-
-type kakaoUserData = {
-  id: string;
-};
 
 export const useKakaoLogin = () => {
   const [kakao, setKakao] = useState<typeof window.Kakao>(null);
@@ -20,41 +14,12 @@ export const useKakaoLogin = () => {
 
   const open = () => {
     kakao?.Auth.authorize({
-      redirectUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_URI,
+      redirectUri: process.env.NEXT_PUBLIC_KAKAO_REDIRECT_FRONT_SERVER_URI_TEST,
     });
   };
 
-  const getUserData = async (): Promise<kakaoUserData> => {
-    const useData = await kakao?.API.request({
-      url: '/v2/user/me',
-    });
-
-    return useData;
+  return {
+    init,
+    open,
   };
-
-  const getAccessToken = async (code: string) => {
-    const { data } = await kakaoAPI.getAccessToken(code);
-
-    return data;
-  };
-
-  const setAccessToken = (accessToken: string) => {
-    kakao?.Auth.setAccessToken(accessToken);
-  };
-
-  const myServiceLogin = (userData: kakaoUserData) => {
-    SignApi.login(userData.id);
-  };
-
-  const kakaoLogin = async (code: string) => {
-    if (!code) return;
-
-    const { access_token } = await getAccessToken(code);
-    setAccessToken(access_token);
-
-    const userData = await getUserData();
-    myServiceLogin(userData);
-  };
-
-  return { init, open, kakaoLogin };
 };
