8f6d1ef637596ca53ee60b3a7e05ba051a1814a9
YeongMin Ahn <ahn0min95@gmail.com>
2023-09-14 22:02:12 +0900

    feat: search page 이동 링크 추가

--- components/BdageTest/index.tsx
+++ components/BdageTest/index.tsx
@@ -1,6 +1,7 @@
 import CommonBadge from '@/components/common/Badge';
 import FlexBox from '@/components/common/FlexBox';
 import CommonIcon from '@/components/common/Icon';
+import Link from 'next/link';
 
 export default function BadgeGroup() {
   return (
@@ -12,13 +13,14 @@ export default function BadgeGroup() {
       >
         편집
       </CommonBadge>
-      <CommonBadge
-        variant="contained"
-        onClick={() => alert('추가버튼 클릭')}
-        leftIcon={<CommonIcon iconName={'add_circle'} />}
-      >
-        추가
-      </CommonBadge>
+      <Link href={'/fires/search'}>
+        <CommonBadge
+          variant="contained"
+          leftIcon={<CommonIcon iconName={'add_circle'} />}
+        >
+          추가
+        </CommonBadge>
+      </Link>
     </FlexBox>
   );
 }
