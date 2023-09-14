faa44f6431912d28c5a545d0e0e37f9d0e620459
YeongMin Ahn <ahn0min95@gmail.com>
2023-09-14 14:20:51 +0900

    feat: Section Component props 변경

--- components/Section/index.tsx
+++ components/Section/index.tsx
@@ -1,12 +1,14 @@
 import styled from '@emotion/styled';
 
-import { CSSProperties, PropsWithChildren } from 'react';
+import type { CSSProperties, PropsWithChildren } from 'react';
 
 interface SectionContainerProps extends PropsWithChildren {
   textAlign?: CSSProperties['textAlign'];
-  paddingTop?: number;
-  paddingBottom?: number;
+  paddingTop?: CSSProperties['paddingTop'];
+  paddingBottom?: CSSProperties['paddingBottom'];
 }
+
+type SectionTitleProps = Pick<SectionContainerProps, 'paddingBottom'>;
 
 function SectionContainer({
   children,
@@ -29,11 +31,12 @@ const S = {
   Container: styled.section<Omit<SectionContainerProps, 'children'>>`
     text-align: ${({ textAlign }) => textAlign};
 
-    padding-top: ${({ paddingTop }) => `${paddingTop}px`};
-    padding-bottom: ${({ paddingBottom }) => `${paddingBottom}px`};
+    padding-top: ${({ paddingTop }) => paddingTop};
+    padding-bottom: ${({ paddingBottom }) => paddingBottom};
   `,
-  SectionTitle: styled.h4`
+  SectionTitle: styled.h4<SectionTitleProps>`
     color: ${({ theme }) => theme.palette.sementic.font_section_title};
+    padding-bottom: ${({ paddingBottom }) => paddingBottom};
   `,
   SectionSubTitle: styled.p``,
 };
