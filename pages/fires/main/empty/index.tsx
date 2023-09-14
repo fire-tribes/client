21b45de7e60e4cae694f60fb3530b17fea3fe845
Whale2200d <skatewang99@gmail.com>
2023-09-14 20:41:33 +0900

    Feat : empty 메인 페이지
    empty 페이지 컴포넌트 (수정)

--- pages/fires/main/empty/index.tsx
+++ pages/fires/main/empty/index.tsx
@@ -1,32 +1,86 @@
-import AlertModal from '@/components/common/AlertModal';
-import Link from 'next/link';
+// import { EmptyUI } from './style';
+import { basic } from '@/styles/palette';
+import CommonButton from '@/components/common/Button/CommonButton';
+// import MoneyBagImage from '@/public/Money_bag.png';
+import CloudImage from '@/public/Cloud.png';
+import PlusSvg from '@/public/icon/plus.svg';
+import styled from '@emotion/styled';
+import { useRouter } from 'next/router';
+import Image from 'next/image';
 
 function Empty() {
+  const router = useRouter();
+  const onMoveSearchPage = () => {
+    router.push('/fires/search');
+  };
   return (
-    <div>
-      <AlertModal
-        type={'confirm'}
-        title={'준비중입니다.'}
-        message={
-          '4대 보험을 제한 결과값 도출은 곧 출시 예정입니다. 조금만 기다려주세요!'
-        }
-      />
-      <AlertModal
-        type={'alert'}
-        title={'최근 검색어 삭제'}
-        message={'최근 검색어를 모두 삭제하시겠어요?'}
-      />
-      <div>
-        <div>돈주머니 이미지</div>
+    <EmptyUI.Container>
+      <EmptyUI.Item>
         <div>
-          <strong>배당 계산을 위해 첫 주식을 추가해주세요.</strong>
+          <Image src={CloudImage} alt="Cloud Image" />
         </div>
-        <Link href="/fires/search">
-          <span>+ 주식 추가하기</span>
-        </Link>
-      </div>
-    </div>
+        <div>
+          배당 계산을 위해
+          <tr /> 첫 주식을 추가해주세요.
+        </div>
+        <div onClick={onMoveSearchPage}>
+          <CommonButton>
+            <Image src={PlusSvg} alt="plus Svg" />
+            <span>주식 추가하기</span>
+          </CommonButton>
+        </div>
+      </EmptyUI.Item>
+    </EmptyUI.Container>
   );
 }
+
+const Container = styled.div`
+  height: 100vh;
+  position: relative;
+`;
+
+const Item = styled.div`
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
+
+  > div {
+    margin-bottom: 20px;
+    text-align: center;
+  }
+  >div: nth-child(2) {
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 700;
+  }
+  > div:last-child {
+    margin-bottom: 0;
+
+    > button {
+      padding: 10px 18px;
+
+      border-radius: 30px;
+      background-color: ${basic.gray1};
+
+      color: ${basic.point_blue02};
+      font-family: Pretendard;
+      font-size: 17px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 26px;
+
+      > span {
+        margin-left: 8px;
+      }
+    }
+  }
+`;
+
+export const EmptyUI = {
+  Container,
+  Item,
+} as const;
 
 export default Empty;
