95ec2f3e62548b6bd442f49cec9ecd846731e363
YeongMin Ahn <gkswn45@naver.com>
2023-09-15 14:46:58 +0900

    feat: 포트폴리오 자산별 디테일 모델 타입 추가

---
+++ @types/models/portfolio.ts
@@ -0,0 +1,15 @@
+// TODO:
+export type MyPortfolioAssetDetailModel = {
+  assetId: number;
+  tickerCode: string;
+  stockCode: string;
+  count: number;
+  averagePrice: string;
+  currentPrice: string;
+  assetPriceChangeRate: string;
+  assetPriceChange: string;
+  value: number;
+  rateOfReturn: number;
+  dividendYield: number;
+  dividendMonth: number[];
+};
