91f7be1548a708b6f57af019413967022e7f17bd
ahn0min <ahn0min95@gmail.com>
2023-09-16 22:12:01 +0900

    fix: withCredentials 로 인행 CORS 에러 해결(#34)

--- core/api/instance.ts
+++ core/api/instance.ts
@@ -1,14 +1,15 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 const createAPIInstance = (config: AxiosRequestConfig) => {
-  const instance = axios.create({ withCredentials: true, ...config });
+  const instance = axios.create({
+    ...config,
+  });
 
   return instance;
 };
 
 const APIInstance = createAPIInstance({
   baseURL: process.env.NEXT_PUBLIC_SERVER_URL + '/api/v1/',
-  withCredentials: true,
 });
 
 export default APIInstance;
