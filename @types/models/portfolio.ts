c1f60f6f89c687b797484aba5d8b9e803c344b80
YeongMin Ahn <ahn0min95@gmail.com>
2023-09-18 15:24:32 +0900

    refactor: 포트폴리오 responseModel에 이해를 위한 주석 작성

--- @types/models/portfolio.ts
+++ @types/models/portfolio.ts
@@ -1,27 +1,25 @@
-// TODO:
-
 import { ExchangeRateSymbol } from '@/@types/models/exchangeRate';
 
 export type MyPortfolioModel = {
   portfolioId: number;
-  totalValue: 0;
-  totalValueChange: 0;
-  totalValueChangeRate: 0;
-  assetDetails: MyportfoliAssetDetailModel;
+  totalValue: number; // 총 자산 가격
+  totalValueChange: number; // 총 자산 변동금액
+  totalValueChangeRate: number; // 총 자산 변동률
+  assetDetails: MyportfoliAssetDetailModel[]; // 자산별 정보
 };
 
 export type MyportfoliAssetDetailModel = {
-  assetId: number;
-  tickerCode: string;
-  stockCode: string;
-  count: number;
-  averagePrice: string;
-  currentPrice: string;
-  assetPriceChangeRate: string;
-  assetPriceChange: string;
-  value: number;
-  rateOfReturn: number;
-  dividendYield: number;
-  dividendMonth: number[];
-  currencyType: ExchangeRateSymbol;
+  assetId: number; // 자산Id
+  tickerCode: string; // 미국주식코드
+  stockCode: string; // 한국주식코드
+  count: number; // 자산수량
+  averagePrice: string; // 평단가
+  currentPrice: string; // 현재가
+  assetPriceChangeRate: string; // 등락율
+  assetPriceChange: string; // 등략
+  value: number; // 자산가치
+  rateOfReturn: number; // 수익률
+  dividendYield: number; // 배당수익률
+  dividendMonth: number[]; // 배당주기
+  currencyType: ExchangeRateSymbol; // 통화
 };
