6a1de834290f135590cef1b09bd0bbc12311b71a
YeongMin Ahn <ahn0min95@gmail.com>
2023-09-18 15:26:19 +0900

    stash: 서버측 에러로 인해mockPortFolioData 기반으로 훅 변경

--- hook/useMyPortFolio.ts
+++ hook/useMyPortFolio.ts
@@ -1,22 +1,80 @@
-// import { useControlMode } from '@/hook/useControlMode';
+import { MyPortfolioModel } from '@/@types/models/portfolio';
 import { useMyPortFolioQuery } from '@/hook/useQueryHook/useMyPortFolioQuery';
-import { useRouter } from 'next/router';
-import { useCallback, useEffect } from 'react';
+// import { useRouter } from 'next/router';
+// import { useCallback, useEffect } from 'react';
+
+const mockMyPortFolioData: MyPortfolioModel = {
+  portfolioId: 2,
+  totalValue: 200,
+  totalValueChange: 100,
+  totalValueChangeRate: 50,
+  assetDetails: [
+    {
+      assetId: 20,
+      tickerCode: 'APPLE',
+      stockCode: '',
+      count: 5,
+      averagePrice: '20',
+      currentPrice: '50',
+      assetPriceChange: '100',
+      assetPriceChangeRate: '100%',
+      value: 200,
+      rateOfReturn: 100,
+      dividendYield: 2,
+      dividendMonth: [1, 5, 7],
+      currencyType: 'USD',
+    },
+  ],
+};
 
 export const useMyPortFolio = (portFolioId?: number) => {
-  const router = useRouter();
+  // const router = useRouter();
   const { data, isLoading } = useMyPortFolioQuery(portFolioId);
+  // const redirect = useCallback(() => router.push('/empty'), [router]);
 
-  const redirect = useCallback(() => router.push('/empty'), [router]);
-
-  useEffect(() => {
-    if (!data && isLoading) {
-      redirect();
-      return;
-    }
-  }, [data, isLoading, redirect]);
+  // useEffect(() => {
+  //   if (!data && isLoading) {
+  //     redirect();
+  //     return;
+  //   }
+  // }, [data, isLoading, redirect]);
 
   // TODO: 심플모드에 따라서 다르게 데이터를 내려주도록
+
+  // const myPortFolioData = data?.data.data;
+  const myPortFolioData = mockMyPortFolioData;
+  if (myPortFolioData) {
+    // const {
+    //   portfolioId,
+    //   totalValue,
+    //   totalValueChange,
+    //   totalValueChangeRate,
+    //   assetDetails,
+    // } = myPortFolioData;
+
+    // const {
+    //   assetId,
+    //   tickerCode,
+    //   stockCode,
+    //   count,
+    //   averagePrice,
+    //   currentPrice,
+    //   assetPriceChangeRate,
+    //   assetPriceChange,
+    //   value,
+    //   rateOfReturn,
+    //   dividendYield,
+    //   dividendMonth,
+    //   currencyType,
+    // } = assetDetails;
+
+    // TODO: 별도의 query로 나누자
+
+    return {
+      data: myPortFolioData,
+      isLoading,
+    };
+  }
 
   return { data, isLoading };
 };
