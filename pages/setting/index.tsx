6069f32679fe2d2f91730658f992b0289f7edeb6
YeongMin Ahn <ahn0min95@gmail.com>
2023-09-19 12:20:54 +0900

    feat: Setting Page UI 구현

--- pages/setting/index.tsx
+++ pages/setting/index.tsx
@@ -1,9 +1,44 @@
+import CommonIcon from '@/components/common/Icon';
 import Layout from '@/components/common/Layout';
+import { StyledHeader, StyledTitle } from '@/pages/setting/styles';
+import {
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+} from '@mui/material';
 
 export default function SettingPage() {
   return (
     <Layout>
-      <div>setting</div>
+      <StyledHeader>
+        <StyledTitle>계산</StyledTitle>
+      </StyledHeader>
+      <List>
+        <ListItem>
+          <ListItemText></ListItemText>
+        </ListItem>
+      </List>
+      <List>
+        <ListItem disablePadding>
+          <ListItemButton sx={{ padding: '14px 0', gap: '10px' }}>
+            <ListItemIcon sx={{ width: '24px', minWidth: '0' }}>
+              <CommonIcon iconName="note" width={24} height={24} />
+            </ListItemIcon>
+            <ListItemText primary="이용약관" />
+          </ListItemButton>
+        </ListItem>
+
+        <ListItem disablePadding>
+          <ListItemButton sx={{ padding: '14px 0', gap: '10px' }}>
+            <ListItemIcon sx={{ width: '24px', minWidth: '0' }}>
+              <CommonIcon iconName="lock" width={24} height={24} />
+            </ListItemIcon>
+            <ListItemText primary="개인정보처리방침" />
+          </ListItemButton>
+        </ListItem>
+      </List>
     </Layout>
   );
 }
