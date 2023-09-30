44e4273c78bf3028a1dc71f64b107da7eb3d6d98
YeongMin Ahn <ahn0min95@gmail.com>
2023-09-30 11:15:42 +0900

    feat: 금액에 따라 단위가 달라지는 함수 추가 , 테스트 추가

--- components/Chart/utils.ts
+++ components/Chart/utils.ts
@@ -27,6 +27,35 @@ const formatChartValue = (value: string | number) => {
   return '';
 };
 
+/** 차트에서 사용되는 util 함수, 한국 기준 앞자리만 반환합니다.
+ * ex) 9999999 => 999만
+ * ex) 999999999 => 9억
+ */
+export const getShortCurrencyKR = (value: number) => {
+  /** 최대 100억 */
+  const TEN_BLIILON = 10000000000;
+
+  // 음수 또는 0
+  if (value <= 0) return '';
+
+  // 1만 미만
+  if (value < 10000) {
+    return value.toString();
+  }
+
+  // 1억 미만
+  if (value < 10000 * 10000) {
+    return Math.floor(value / 10000) + '만';
+  }
+
+  /** 100억 미만 */
+  if (value < TEN_BLIILON) {
+    return Math.floor(value / (10000 * 10000)) + '억';
+  }
+
+  return '99억';
+};
+
 const createShowChartDividendDatas = (
   dividendDatas: MonthlyDividends | undefined,
 ) => {
---
+++ test/utils.test.ts
@@ -0,0 +1,81 @@
+import { getShortCurrencyKR } from '../components/Chart/utils';
+
+describe('new test', () => {
+  it('-100', () => {
+    const result = getShortCurrencyKR(-100);
+
+    expect(result).toBe('');
+  });
+
+  it('0', () => {
+    const result = getShortCurrencyKR(0);
+
+    expect(result).toBe('');
+  });
+
+  it('100', () => {
+    const result = getShortCurrencyKR(100);
+
+    expect(result).toBe('100');
+  });
+
+  it('1,000', () => {
+    const result = getShortCurrencyKR(1000);
+
+    expect(result).toBe('1000');
+  });
+
+  it('9,999', () => {
+    const result = getShortCurrencyKR(9999);
+
+    expect(result).toBe('9999');
+  });
+
+  it('1만', () => {
+    const result = getShortCurrencyKR(10000);
+
+    expect(result).toBe('1만');
+  });
+
+  it('9,999,999', () => {
+    const result = getShortCurrencyKR(9999999);
+
+    expect(result).toBe('999만');
+  });
+
+  it('99,999,999', () => {
+    const result = getShortCurrencyKR(99999999);
+
+    expect(result).toBe('9999만');
+  });
+
+  it('1억', () => {
+    const result = getShortCurrencyKR(100000000);
+
+    expect(result).toBe('1억');
+  });
+
+  it('13억3천', () => {
+    const result = getShortCurrencyKR(1330000000);
+
+    expect(result).toBe('13억');
+  });
+
+  it('100억 - 1원', () => {
+    const result = getShortCurrencyKR(10000000000 - 1);
+
+    expect(result).toBe('99억');
+  });
+
+  it('100억', () => {
+    const result = getShortCurrencyKR(10000000000);
+
+    expect(result).toBe('99억');
+  });
+
+  it('133억3천', () => {
+    const result = getShortCurrencyKR(13330000000);
+
+    expect(result).toBe('99억');
+  });
+});
