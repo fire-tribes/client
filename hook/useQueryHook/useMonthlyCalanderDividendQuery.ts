18a3d8073ba6892ea7773afafd475b267fc8a8e6
YeongMin Ahn <ahn0min95@gmail.com>
2023-10-03 20:01:49 +0900

    feat: 메인페이지 월배당 캘린더 부분 기능 수정

--- hook/useQueryHook/useMonthlyCalanderDividendQuery.ts
+++ hook/useQueryHook/useMonthlyCalanderDividendQuery.ts
@@ -1,33 +1,66 @@
+import { DividendCalanderModel } from '@/@types/models/dividend';
+import { ResponseSuccess } from '@/@types/models/response';
+import { getShortCurrencyKR } from '@/components/Chart/utils';
 import { dividendAPI } from '@/core/api/dividend';
 import { useControlMode } from '@/hook/useControlMode';
 import { useExchangeRate } from '@/hook/useExchangeRate';
 import { queryKeys } from '@/hook/useQueryHook/queryKeys';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 
 export const useMonthlyCalanderDividendQuery = () => {
-  const { data } = useQuery(
-    queryKeys.monthlyCalanderDividend(),
-    dividendAPI.getCalenderDividend,
-  );
-  return { ...data?.data };
-};
-
-export const useMonthlyCalanderDividendExchangeQuery = () => {
-  const { data: datas } = useMonthlyCalanderDividendQuery();
+  const queryClient = useQueryClient();
   const { exchangeRate } = useExchangeRate();
   const { modeData } = useControlMode();
 
   return useQuery(
-    queryKeys.monthlyCalanderDividend(modeData.isSimple, exchangeRate),
-    () => {
-      if (datas?.length && exchangeRate) {
-        return datas.map((data) => ({
-          ...data,
-          expectedDividends: data.expectedDividends * exchangeRate,
-        }));
-      }
-
-      return datas;
+    queryKeys.monthlyCalanderDividend(),
+    () => dividendAPI.getCalenderDividend(),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(
+          queryKeys.monthlyCalanderDividend(modeData.isSimple, exchangeRate),
+        );
+      },
     },
+  );
+};
+
+export const useMonthlyCalanderDividendExchangeQuery = () => {
+  useMonthlyCalanderDividendQuery();
+  const queryClient = useQueryClient();
+  const { exchangeRate } = useExchangeRate();
+  const { modeData } = useControlMode();
+
+  const getQueryFunction = () => {
+    const monthlyCalanderDividendData:
+      | ResponseSuccess<DividendCalanderModel[]>
+      | undefined = queryClient.getQueryData(
+      queryKeys.monthlyCalanderDividend(),
+    );
+
+    const calanderDividendDatas = monthlyCalanderDividendData?.data;
+
+    if (calanderDividendDatas?.length && exchangeRate) {
+      return {
+        ...monthlyCalanderDividendData,
+        data: calanderDividendDatas?.map((data) => ({
+          ...data,
+          expectedDividends: modeData.isSimple
+            ? getShortCurrencyKR(
+                Math.floor(data.expectedDividends * exchangeRate),
+              ) + '원'
+            : Math.floor(data.expectedDividends * exchangeRate).toLocaleString(
+                'ko-kr',
+              ) + '원',
+        })),
+      };
+    }
+
+    return monthlyCalanderDividendData;
+  };
+
+  return useQuery(
+    queryKeys.monthlyCalanderDividend(modeData.isSimple, exchangeRate),
+    getQueryFunction,
   );
 };
