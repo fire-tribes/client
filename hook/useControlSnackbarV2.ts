c688fd2e840c7dcf7f13d1fe81e3ec75a252b0cd
YeongMin Ahn <ahn0min95@gmail.com>
2023-10-24 02:08:26 +0900

    fix: merge 과정 resetSnackbar 주석처리된 이슈 해결

--- hook/useControlSnackbarV2.ts
+++ hook/useControlSnackbarV2.ts
@@ -25,9 +25,9 @@ export const useControlSnackbarV2 = () => {
     setSnackbarState((pre) => ({ ...pre, open: false }));
   };
 
-  // const resetSnackbar = () => {
-  //   setSnackbarState({ ...snackbarAtomDefault });
-  // };
+  const resetSnackbar = () => {
+    setSnackbarState({ ...snackbarAtomDefault });
+  };
 
   // useEffect(() => {
   //   return () => resetSnackbar();
