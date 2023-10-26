5a0cd4cb47b632d3d83bbc8f642bc651476cc1de
ahn0min <ahn0min95@gmail.com>
2023-10-26 16:14:28 +0900

    feat: amplitude init을 개발용, 프로덕션용 분리

--- hook/useAmplitudeInit.ts
+++ hook/useAmplitudeInit.ts
@@ -4,16 +4,39 @@ import * as amplitude from '@amplitude/analytics-browser';
 /** AmpliTude Event를 import 하는 방법 */
 // import { TrackEvent } from '@amplitude/analytics-types';
 
-const AMPLITUDE_KEY = process.env.NEXT_PUBLIC_AMPLITUDE_API_KEY as string;
+const AMPLITUDE_PRODUCT_KEY = process.env
+  .NEXT_PUBLIC_AMPLITUDE_PRODUCT_API_KEY as string;
+const AMPLITUDE_DEV_KEY = process.env
+  .NEXT_PUBLIC_AMPLITUDE_DEV_API_KEY as string;
 
 //TODO: 매번 init을 해주지 않으려면 Provider를 만들어서 Context로 해줘야하지 않을까?
 export const useAmplitudeInit = () => {
   useEffect(() => {
-    // awiat 로 불러오기
-    // amplitude = await import("@amplitude/analytics-browser")
-    amplitude.init(AMPLITUDE_KEY, {
-      defaultTracking: true,
-    });
+    const origin = window.location.origin;
+
+    if (process.env.NEXT_PUBLIC_LOCAL_SERVER_ORIGIN === origin) {
+      amplitude.init(AMPLITUDE_DEV_KEY, {
+        defaultTracking: true,
+      });
+
+      return;
+    }
+
+    if (process.env.NEXT_PUBLIC_DEV_SERVER_ORIGIN === origin) {
+      amplitude.init(AMPLITUDE_DEV_KEY, {
+        defaultTracking: true,
+      });
+
+      return;
+    }
+
+    if (process.env.NEXT_PUBLIC_PRODUCT_SERVER_ORIGIN === origin) {
+      amplitude.init(AMPLITUDE_PRODUCT_KEY, {
+        defaultTracking: true,
+      });
+
+      return;
+    }
   }, []);
 
   return {
