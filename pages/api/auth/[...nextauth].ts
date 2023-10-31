401e1b7ef0c2830fbfd901426384488165187ba5
YeongMin Ahn <ahn0min95@gmail.com>
2023-10-31 18:32:35 +0900

    [FEAT] Next-Auth 기반으로 Oauth 로그인 구현(Kakao, Google) (#152)
    * feat: google 기본 template 불러오기
    
    * feat: logout 과정 중 next-auth signOut 함수 호출 추가
    
    * feat: next-auth를 이용할 경우 불필요해지는 kakao, google 파일제거
    
    * feat: 로그인이 불필요할 경우 로그인 페이지 접속 거부
    
    * chore: install to "next-auth^4.24.4"
    
    * feat: next-auth handler 세텅
    
    * feat: next-auth를 이용항 Oauth 기반 로그인 구현
    
    * refactor: next-auth 기반 Oauth 로그인 작업 적용으로 인한 불필요해진 파일 제거
    
    * feat: Add Google, Kakao Login Button

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,38 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import KakaoProvider from 'next-auth/providers/kakao';
+
+export default NextAuth({
+  useSecureCookies: process.env.NODE_ENV === 'production' ? true : false,
+  session: {
+    maxAge: 60,
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_OAUTH_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET_KEY || '',
+      // allowDangerousEmailAccountLinking: true,
+    }),
+    KakaoProvider({
+      clientId: process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY || '',
+      clientSecret: process.env.KAKAO_SECRET_KEY || '',
+    }),
+  ],
+  callbacks: {
+    async session({ session, token }) {
+      return {
+        ...session,
+        provider: token.provider || '',
+      };
+    },
+    async jwt({ token, account }) {
+      if (account) {
+        token.provider = account.provider;
+      }
+      return token;
+    },
+  },
+  pages: {
+    signIn: '/login',
+  },
+});
