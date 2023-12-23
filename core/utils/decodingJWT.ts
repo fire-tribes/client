6961bf8bffb423ee1225d99ffed4e0544b05363e
YeongMin Ahn <ahn0min95@gmail.com>
2023-12-24 01:16:58 +0900

    Feat/slack error alarm bot (#179)
    * feat: send api error alarm to slack
    
    * feat: send sign api alarm to slack

--- core/utils/decodingJWT.ts
+++ core/utils/decodingJWT.ts
@@ -5,18 +5,23 @@ export function decodingJWT<T>(JWTName: string): T | undefined {
   const JWT = cookie.get(JWTName);
 
   if (JWT) {
-    /**
-     * [0] : header
-     * [1] : payload
-     * [2] : VERIFY
-     */
-    const base64Payload = JWT.split('.')[1];
-    const payload = Buffer.from(base64Payload, 'base64');
-    /** 문자열 변환 후 JS Object로 변환 */
-    const result: T = JSON.parse(payload.toString());
-
-    return result;
+    return parsingJWT(JWT);
   }
 
   return undefined;
+}
+
+export function parsingJWT<T>(JWT: string) {
+  /**
+   * [0] : header
+   * [1] : payload
+   * [2] : VERIFY
+   */
+
+  const base64Payload = JWT.split('.')[1];
+  const payload = Buffer.from(base64Payload, 'base64');
+
+  /** 문자열 변환 후 JS Object로 변환 */
+  const result: T = JSON.parse(payload.toString());
+  return result;
 }
--- pages/api/alarm/slack.ts
+++ pages/api/alarm/slack.ts
@@ -1,36 +1,39 @@
 import { SlackAPIInstance, isProductionServer } from '@/core/api/instance';
+import { parsingJWT } from '@/core/utils/decodingJWT';
+import { AccessTokenDecodingResult } from '@/hook/useDecodingAccessToken';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
-  console.log(req.headers.referer);
-
-  const requestHostname = req.headers.host?.split(':')[0] || '';
-
   if (req.method === 'POST') {
-    if (isProductionServer(requestHostname)) {
-      if (req.body) {
-        const postResult = await SlackAPIInstance.post('', {
-          text: 'snowball.watch API Error 발생',
-          blocks: [
-            {
-              type: 'section',
-              block_id: '2',
-              text: {
-                type: 'mrkdwn',
-                text:
-                  '>❌ API 에러 발생\n' +
-                  `*발생지점* : ${req.headers.referer}` +
-                  req.body.message,
-              },
-            },
-          ],
-        });
+    const requestHostname = req.headers.host?.split(':')[0] || '';
+    if (isProductionServer(requestHostname) && req.body) {
+      const accessToken = req.cookies.accessToken || '';
+      const parsedJWTPayload =
+        parsingJWT<AccessTokenDecodingResult>(accessToken);
+      const email = parsedJWTPayload?.email;
 
-        return res.send(postResult.data);
-      }
+      const postResult = await SlackAPIInstance.post('', {
+        text: 'snowball.watch API Error 발생',
+        blocks: [
+          {
+            type: 'section',
+            block_id: '2',
+            text: {
+              type: 'mrkdwn',
+              text:
+                '>❌ API 에러 발생\n' +
+                `*발생지점* : ${req.headers.referer}` +
+                req.body.message +
+                `\n*유저 정보* : email: ${email}`,
+            },
+          },
+        ],
+      });
+
+      return res.send(postResult.data);
     }
   }
 
